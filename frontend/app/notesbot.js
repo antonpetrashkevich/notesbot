8b0dda90fcd042cdb1fbfe62def0fa6dba1e1767
n1 <warr1or@inbox.ru>
2025-02-10 21:55:10 +0300

    login, setup key, validate key

--- frontend/app/notesbot.js
+++ frontend/app/notesbot.js
@@ -1,62 +1,100 @@
 import { appName, appState, widgets, pageWidget, colors, icons, updatePage, goTo, startApp, modalOn, modalOff, widget, templateWidget, row, column, grid, text, textLink, image, svg, canvas, video, youtubeVideo, button, select, input, textArea, hint, notification, imageInput, loadingPage, notFoundPage, generalErrorPage, fixedHeader, menu, base } from '/home/n1/projects/profiler/frontend/apex.js';
+import { collection, doc, serverTimestamp, runTransaction, getDoc, getDocs, getDocFromCache, addDoc, setDoc, updateDoc } from "firebase/firestore";
 
 
 export const styles = {
     card: {
-        padding: '0.65rem',
+        padding: '0.75rem',
         borderRadius: '0.5rem',
         borderStyle: 'solid',
         borderWidth: '1px',
         borderColor: colors.gray[300],
     },
     filledCard: {
-        padding: '0.65rem',
+        padding: '0.75rem',
         borderRadius: '0.5rem',
         backgroundColor: colors.gray[100],
     },
-    tag: {
-        padding: '0.25rem 0.5rem',
-        borderRadius: '0.25rem',
-        backgroundColor: colors.cyan[200],
-        fontWeight: 600,
-        color: colors.cyan[900]
-    },
     actionButton: {
         padding: '0.65rem',
+        justifyContent: 'center',
         backgroundColor: colors.blue[600],
         hoverColor: colors.blue[700],
         color: 'white',
     },
-    actionTextButton: {
-        padding: '0.5rem',
-        color: colors.blue[600],
-        hoverColor: colors.blue[100],
+    actionSecondaryButton: {
+        padding: '0.65rem',
+        justifyContent: 'center',
+        backgroundColor: 'inherit',
+        hoverColor: colors.gray[100],
     },
     dangerButton: {
-        padding: '0.65rem',
         backgroundColor: colors.red[600],
         hoverColor: colors.red[700],
         color: 'white',
-    },
-    premiumButton: {
-        backgroundColor: colors.yellow[400],
-        hoverColor: colors.yellow[500],
-        fontWeight: 600,
-        color: colors.yellow[900],
-    },
-    disabledButton: {
-        padding: '0.5rem',
-        borderRadius: '0.5rem',
-        borderStyle: 'solid',
-        borderWidth: '1px',
-        borderColor: colors.gray[300],
-        fontSize: '1rem',
-        lineHeight: 1,
-        color: colors.gray[500],
-        cursor: 'not-allowed',
-        userSelect: 'none',
-        webkitTapHighlightColor: 'transparent',
-    },
+    }
+}
+
+
+export function toBase64(uint8Array) {
+    let binary = '';
+    uint8Array.forEach(byte => {
+        binary += String.fromCharCode(byte);
+    });
+    return btoa(binary);
+}
+export function fromBase64(base64) {
+    const binaryString = atob(base64);
+    const uint8Array = new Uint8Array(binaryString.length);
+    for (let i = 0; i < binaryString.length; i++) {
+        uint8Array[i] = binaryString.charCodeAt(i);
+    }
+    return uint8Array;
+}
+
+
+export async function generateKey(keyphrase, salt) {
+    const keyMaterial = await window.crypto.subtle.importKey(
+        'raw',
+        appState.textEncoder.encode(keyphrase),
+        { name: 'PBKDF2' },
+        false,
+        ['deriveKey']
+    );
+    return window.crypto.subtle.deriveKey(
+        {
+            name: 'PBKDF2',
+            salt: fromBase64(salt),
+            iterations: 2 ** 16,
+            hash: 'SHA-256',
+        },
+        keyMaterial,
+        { name: 'AES-GCM', length: 256 },
+        true,
+        ['encrypt', 'decrypt']
+    );
+}
+export async function encrypt(key, data) {
+    const iv = window.crypto.getRandomValues(new Uint8Array(12));
+    const encryptedData = await window.crypto.subtle.encrypt(
+        {
+            name: "AES-GCM",
+            iv: iv,
+        },
+        key,
+        data
+    );
+    return { iv: toBase64(iv), data: toBase64(new Uint8Array(encryptedData)) };
+}
+export async function decrypt(key, ivdata) {
+    return await window.crypto.subtle.decrypt(
+        {
+            name: "AES-GCM",
+            iv: fromBase64(ivdata.iv),
+        },
+        key,
+        fromBase64(ivdata.data)
+    );
 }
 
 
@@ -92,30 +130,164 @@ export function loginPage() {
 
 export function setupPage() {
     return {
-        widget: row({
-            width: '100%',
-            height: '100%',
-            justifyContent: 'center',
-            alignItems: 'center',
-            gap: '1rem',
+        widget: base({
             children: [
-                input({
-                    id: 'e2ekey-input',
-                    width: '16rem',
-                    attributes: {
-                        placeholder: 'Key phrase'
-                    }
-                }),
-                button({
-                    ...styles.actionButton,
-                    text: 'Set',
-                    click: function (event) {
+                text({ marginTop: '2rem', fontSize: '1.5rem', fontWeight: 600, text: 'Keyphrase' }),
+                text({ marginTop: '1rem', text: 'Your data is encrypted with a keyphrase using end-to-end encryption. Only you can decrypt it. Store your keyphrase securely — if it\'s lost, you won\'t be able to recover your data' }),
+                column({
+                    marginTop: '4rem',
+                    width: '100%',
+                    flexGrow: 1,
+                    alignSelf: 'center',
+                    justifyContent: 'center',
+                    alignItems: 'start',
+                    children: [
+                        text({ fontWeight: 600, text: 'Your keyphrase' }),
+                        hint(() => ({ id: 'keyphrase-hint', marginTop: '0.5rem', normalText: 'Don\'t use easy-to-guess combinations like \'password\', \'12345\' and so on', errorText: 'Required. Don\'t use easy-to-guess combinations like \'password\', \'12345\' and so on' }), true),
+                        input({ id: 'keyphrase-input', width: '100%', marginTop: '0.5rem', attributes: { type: 'password', maxlength: '64', placeholder: Array.from({ length: 16 }, () => String.fromCharCode(Math.floor(Math.random() * 94) + 33)).join('') } }),
 
-                    }
-                })
-            ]
+                        text({ marginTop: '2rem', fontWeight: 600, text: 'Repeat keyphrase' }),
+                        hint(() => ({ id: 'keyphrase-repeat-hint', marginTop: '0.5rem', errorText: 'Doesn\'t match' }), true),
+                        input({ id: 'keyphrase-repeat-input', width: '100%', marginTop: '0.5rem', attributes: { type: 'password', maxlength: '64' } }),
+                    ]
+                }),
+            ],
+            footer: row({
+                margin: '0.5rem',
+                width: 'calc(min(100%, 800px) - 1rem)',
+                gap: '1rem',
+                justifyContent: 'end',
+                alignItems: 'center',
+                children: [
+                    button({
+                        flexGrow: window.innerWidth <= 768 ? 1 : undefined,
+                        ...styles.actionSecondaryButton,
+                        click: function (event) {
+                            appState.firebase.signOut(appState.firebase.auth);
+                        },
+                        text: 'Cancel'
+                    }),
+                    button({
+                        flexGrow: window.innerWidth <= 768 ? 1 : undefined,
+                        ...styles.actionButton,
+                        click: async function (event) {
+                            if (!widgets['keyphrase-input'].domElement.value) {
+                                widgets['keyphrase-hint'].update(false);
+                                window.scrollTo(0, 0);
+                                return;
+                            }
+                            widgets['keyphrase-hint'].update(true);
+                            if (!widgets['keyphrase-repeat-input'].domElement.value || widgets['keyphrase-input'].domElement.value != widgets['keyphrase-repeat-input'].domElement.value) {
+                                widgets['keyphrase-repeat-hint'].update(false);
+                                window.scrollTo(0, 0);
+                                return;
+                            }
+                            updatePage(loadingPage());
+                            try {
+                                const salt = toBase64(window.crypto.getRandomValues(new Uint8Array(16)));
+                                const key = await generateKey(widgets['keyphrase-input'].domElement.value, salt);
+                                const keytest = await encrypt(key, appState.textEncoder.encode('XPL'));
+                                const userDocRef = doc(appState.firebase.firestore, 'users', appState.user.uid);
+                                const txResult = await runTransaction(appState.firebase.firestore, async (transaction) => {
+                                    const userDoc = await transaction.get(userDocRef);
+                                    if (userDoc.exists()) {
+                                        throw "User already exists";
+                                    }
+                                    transaction.set(userDocRef, {
+                                        registerTimestamp: serverTimestamp(),
+                                        salt,
+                                        keytest
+                                    });
+                                    return true;
+                                });
+                                appState.key = key;
+                                window.localStorage.setItem('keyphrase', widgets['keyphrase-input'].domElement.value);
+                                updatePage(homePage());
+                            } catch (error) {
+                                console.error(error);
+                                updatePage(generalErrorPage());
+                            }
+                        },
+                        children: [
+                            text({ text: 'Save' })
+                        ]
+                    })
+                ]
+            })
         }),
-        meta: { title: `Setup | ${appName}`, description: 'Setup pgae.' }
+        meta: { title: `Setup | ${appName}`, description: 'Setup page.' }
+    };
+}
+
+
+export function keyphrasePage(salt, keytest) {
+    return {
+        widget: base({
+            children: [
+                text({ marginTop: '2rem', fontSize: '1.5rem', fontWeight: 600, text: 'Keyphrase' }),
+                text({ marginTop: '1rem', text: 'Provide keyphrase to decrypt your data' }),
+                column({
+                    marginTop: '4rem',
+                    width: '100%',
+                    flexGrow: 1,
+                    alignSelf: 'center',
+                    justifyContent: 'center',
+                    alignItems: 'start',
+                    children: [
+                        text({ fontWeight: 600, text: 'Your keyphrase' }),
+                        hint(() => ({ id: 'keyphrase-hint', marginTop: '0.5rem', errorText: 'Wrong' }), true),
+                        input({ id: 'keyphrase-input', width: '100%', marginTop: '0.5rem', attributes: { type: 'password', maxlength: '64' } }),
+                    ]
+                }),
+            ],
+            footer: row({
+                margin: '0.5rem',
+                width: 'calc(min(100%, 800px) - 1rem)',
+                gap: '1rem',
+                justifyContent: 'end',
+                alignItems: 'center',
+                children: [
+                    button({
+                        flexGrow: window.innerWidth <= 768 ? 1 : undefined,
+                        ...styles.actionSecondaryButton,
+                        click: function (event) {
+                            appState.firebase.signOut(appState.firebase.auth);
+                        },
+                        text: 'Cancel'
+                    }),
+                    button({
+                        flexGrow: window.innerWidth <= 768 ? 1 : undefined,
+                        ...styles.actionButton,
+                        click: async function (event) {
+                            if (!widgets['keyphrase-input'].domElement.value) {
+                                widgets['keyphrase-hint'].update(false);
+                                window.scrollTo(0, 0);
+                                return;
+                            }
+                            try {
+                                const key = await generateKey(widgets['keyphrase-input'].domElement.value, salt);
+                                await decrypt(key, keytest);
+                                appState.key = key;
+                                window.localStorage.setItem('keyphrase', widgets['keyphrase-input'].domElement.value);
+                                updatePage(homePage());
+                            } catch (error) {
+                                if (error instanceof DOMException && error.name === "OperationError") {
+                                    widgets['keyphrase-hint'].update(false);
+                                    window.scrollTo(0, 0);
+                                } else {
+                                    console.error(error);
+                                    updatePage(generalErrorPage());
+                                }
+                            }
+                        },
+                        children: [
+                            text({ text: 'Save' })
+                        ]
+                    })
+                ]
+            })
+        }),
+        meta: { title: `Keyphrase | ${appName}`, description: 'Keyphrase page.' }
     };
 }
 
