a7e6defc81e76d0dccd73bc5c7cabf72cf7c8be1
n1 <warr1or@inbox.ru>
2025-02-20 21:41:07 +0300

    manifest light theme color

--- frontend/app/notesbot.js
+++ frontend/app/notesbot.js
@@ -27,7 +27,7 @@ export const icons = {
     upload: '<svg viewBox="0 -960 960 960"><path d="M440-200h80v-167l64 64 56-57-160-160-160 160 57 56 63-63v167ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z"/></svg>',
 }
 export const lightTheme = {
-    '--meta-theme-color': '#ffffff',
+    '--meta-theme-color': '#f9fafb',
     '--font-family': 'system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", "Noto Sans", "Liberation Sans", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
     '--mono-font-family': 'SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
     '--shadow-1': '0 1px 2px rgba(0, 0, 0, 0.1)',
